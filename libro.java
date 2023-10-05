2a35eb927590b5dd155bec3f5584781963702d24
valIGi <141454512+valIGi@users.noreply.github.com>
2023-10-04 20:12:33 -0500

    libro.java

--- libro.java
+++ libro.java
@@ -1,3 +1,53 @@
 public class libro {
+/**
+ * clase Libro
+ */
+public class Libro {
+    /**
+     * Atributos
+     */
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+    /**
+     * Mètodos
+     */
+    /**
+     * Constructor para inicializar los atributos
+     *
+     * @param titulo
+     * @param autor
+     * @param numeroPaginas
+     *
+     * Complejidad temporal: 0(1) tiempo constante
+     */
+    public Libro(String titulo, String autor, int numeroPaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
 
+    /**
+     * Mètodo getter  permite obtener el título del libro
+     * @return título del libro
+     *
+     * complejidad temporal: 0(1) tiempo constante
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+    /**
+     * Mètodo getter permite obtener el autor del libro
+     * @return título del libro
+     *
+     * complejidad temporal: 0(1) tiempo constante
+     */
+    public String getAutor() {
+        return autor;
+    }
+    /**
+     * Mètodo getter permite obtener el número de páginas del libro
+     * @return título del libro
+     *
+     * complejidad temporal: 0(1) tiempo constante
 }
